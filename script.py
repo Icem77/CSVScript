d35ba7396a6312c6eb5e2b27ae2f53c490aed488
Icem77 <szymon.kruk787@gmail.com>
2024-11-02 10:41:09 +0100

    Script description update

--- script.py
+++ script.py
@@ -5,8 +5,12 @@ import file_browsing
 
 def main(days=["pn", "wt", "śr", "czw", "pt", "sob", "nd"]):
 
+    # create args parser
     parser = argparse.ArgumentParser(
-        description="Script for reading and writing CSV files in structure of catalogues [miesiąc]/[dzień]/[pora_dnia]."
+        description="""Script for reading/writing CSV files in structure of catalogues [miesiąc]/[dzień]/[pora_dnia].
+                        Script performs only one type of action per run, it's not possible to write AND read at the same time.
+                        If you try to read data from directories/files that do not exist, you will be notified, default value of 0 will be used
+                        for those empty directories/files but the directory/file will NOT be created."""
     )
 
     parser.add_argument(
@@ -27,31 +31,31 @@ def main(days=["pn", "wt", "śr", "czw", "pt", "sob", "nd"]):
     parser.add_argument(
         '-p', '--pory',
         nargs='+',
-        help="For each given day insert the time of the day [r(rano)/w(wieczór)], default will be 'r'."
+        help="For each given day insert the time of the day [r/w], default will be 'r'."
     )
 
     parser.add_argument(
         '-t', '--tworzenie',
         action='store_true',
-        help="Specifies that we want to create files at given directories"
+        help="Specifies that you want to create files at given directories."
     )
 
     parser.add_argument(
         '-o', '--odczytywanie',
         action='store_true',
-        help="Specifies that we want to read files from given directories"
+        help="Specifies that you want to read files from given directories."
     )
 
     args = parser.parse_args()
 
+    # validate script parameters and turn them into triples from which directories will be created
     if (len(args.miesiace) != len(args.dni)):
-        print("Amount of months is not equal to amount of days/days ranges.")
+        print("Amount of given months is not equal to amount of given days and days ranges.")
         sys.exit(1)
 
     if args.pory is None:
         args.pory = []
 
-    #preprocessing on script parameters to create file paths
     triples_to_paths = []
     for index in range(len(args.miesiace)):
         days_in_range = []
@@ -78,6 +82,7 @@ def main(days=["pn", "wt", "śr", "czw", "pt", "sob", "nd"]):
             else:
                 triples_to_paths.append((args.miesiace[index], day, "r"))
 
+    # perfom choosen action on given directories
     if (args.tworzenie and args.odczytywanie):
         print("Can't write AND read from files, choose exactly one action.")
         sys.exit(1)
@@ -88,7 +93,7 @@ def main(days=["pn", "wt", "śr", "czw", "pt", "sob", "nd"]):
         file_browsing.check_and_sum(triples_to_paths)
         #read files from directories
     else:
-        print("Action on directories was not specified, choose one flag from -t and -o.")
+        print("Action on directories was not specified, choose exaclty one flag from -t and -o.")
         sys.exit(1)
 
 
